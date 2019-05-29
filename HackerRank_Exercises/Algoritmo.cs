7be40ed23d37aff9e86d8f997295ab2748366675
Camilo Serrano Abril <PIIP06@dnp.gov.co>
2019-05-29 11:30:17 -0500

    Add project files.

---
+++ HackerRank_Exercises/Algoritmo.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank_Exercises
+{
+    public class Algoritmo
+    {
+
+        public string[] testBalancedBrackets(string[] values)
+        {
+            string[] retorno = new string[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                int checkValue = 0;
+                bool huboError = false;
+                for (int j = 0; j < values[i].Length; j++)
+                {
+                    try
+                    {
+                        if (values[i][j] == '(' && (values[i][j + 1] == '}' || values[i][j + 1] == ']'))
+                        {
+                            retorno[i] = "NO";
+                            huboError = true;
+                            break;
+                        }
+                        else if (values[i][j] == '[' && (values[i][j + 1] == ')' || values[i][j + 1] == '}'))
+                        {
+                            retorno[i] = "NO";
+                            huboError = true;
+                            break;
+                        }
+                        else if (values[i][j] == '{' && (values[i][j + 1] == ')' || values[i][j + 1] == ']'))
+                        {
+                            retorno[i] = "NO";
+                            huboError = true;
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        retorno[i] = "NO";
+                        huboError = true;
+                        break;
+                    }
+                    if (!huboError)
+                    {
+                        if (values[i][j] == '(' || values[i][j] == '[' || values[i][j] == '{')
+                            checkValue++;
+                        if (values[i][j] == '}' || values[i][j] == ']' || values[i][j] == ')')
+                            checkValue--;
+                        if (checkValue < 0)
+                        {
+                            retorno[i] = "NO";
+                            break;
+                        }
+                    }
+                }
+
+                if (!huboError)
+                    retorno[i] = checkValue == 0 ? "YES" : "NO";
+            }
+            return retorno;
+        }
+
+        public int equalStacks(int[] h1, int[] h2, int[] h3)
+        {
+
+            var totalH1 = 0;
+            var totalH2 = 0;
+            var totalH3 = 0;
+
+            List<int> sumasH1 = new List<int>();
+            List<int> sumasH2 = new List<int>();
+            List<int> sumasH3 = new List<int>();
+
+            for (int i = h1.Length - 1; i >= 0; i--)
+            {
+                totalH1 += h1[i];
+                sumasH1.Add(totalH1);
+            }
+            for (int i = h2.Length - 1; i >= 0; i--)
+            {
+                totalH2 += h2[i];
+                sumasH2.Add(totalH2);
+            }
+            for (int i = h3.Length - 1; i >= 0; i--)
+            {
+                totalH3 += h3[i];
+                sumasH3.Add(totalH3);
+            }
+
+            if (totalH1 == totalH2 && totalH2 == totalH3)
+                return totalH1;
+
+            sumasH1.Sort((a, b) => -1 * a.CompareTo(b));
+            sumasH2.Sort((a, b) => -1 * a.CompareTo(b));
+            sumasH3.Sort((a, b) => -1 * a.CompareTo(b));
+
+            var dicH1 = sumasH1.ToDictionary(k => k);
+            var dicH2 = sumasH2.ToDictionary(k => k);
+            var dicH3 = sumasH3.ToDictionary(k => k);
+
+
+            if (sumasH1.Count <= sumasH2.Count && sumasH1.Count <= sumasH3.Count )
+            {
+                foreach (var sumah1 in sumasH1)
+                {
+                    if (dicH2.ContainsKey(sumah1))
+                    {
+                        if (dicH3.ContainsKey(sumah1))
+                        {
+                            return sumah1;
+                        }
+                    }
+                }
+            }
+            if (sumasH2.Count <= sumasH1.Count && sumasH2.Count <= sumasH3.Count )
+            {
+                foreach (var sumah2 in sumasH2)
+                {
+                    if (dicH1.ContainsKey(sumah2))
+                    {
+                        if (dicH3.ContainsKey(sumah2))
+                        {
+                            return sumah2;
+                        }
+                    }
+                }
+            }
+            if (sumasH3.Count <= sumasH1.Count && sumasH3.Count <= sumasH2.Count  )
+            {
+                foreach (var sumah3 in sumasH3)
+                {
+                    if (dicH1.ContainsKey(sumah3))
+                    {
+                        if (dicH2.ContainsKey(sumah3))
+                        {
+                            return sumah3;
+                        }
+                    }
+                }
+            }                           
+            return 0;
+        }
+    }
+}
---
+++ UnitTest_HackerRank/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_HackerRank")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_HackerRank")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df162ff8-e50b-4f4a-83ce-ab0101b3097d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
